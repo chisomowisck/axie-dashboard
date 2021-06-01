81dbd40a3b11a8bbc7f4797600bbaadeb2429e40
Chisomo Wisck <chisomowisck@gmail.com>
2021-06-01 14:23:33 +0200

    initial commit 3

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const serveStatic = require('serve-static');
-const path = require('path');
-
-const app = express();
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
 
-//serve files from dist
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+const app = express()
+
+
+
+app.use('/',serveStatic(path.join(__dirname,'/dist')))
+
 
 const port = process.env.PORT || 8080
-app.listen(port)
 
-console.log('Server Listening on port:' + port)
+app.listen(port)
